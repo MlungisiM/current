0c45399980f3f6463b8ccbf058dc756eacecc62e
CGEJR <mlungisi.mbele@cgic.co.za>
2025-07-31 17:25:39 +0200

    * troubleshooting why the test is passing locally but failing on the pipeline

--- src/test/java/actions/login_actions.java
+++ src/test/java/actions/login_actions.java
@@ -2,7 +2,13 @@ package actions;
 
 import base.base_class;
 import factory.DriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.login_page;
+
+import java.time.Duration;
 
 public class login_actions extends base_class {
 
@@ -16,22 +22,30 @@ public class login_actions extends base_class {
     }
 
     public void login() {
-        String username = DriverFactory.prop.getProperty("username");
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        WebElement usernameInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("frmlogin:username")));
+        usernameInput.sendKeys("TE000159");
+        _login_page.login_button.click();
+
+
+
+        //        String username = DriverFactory.prop.getProperty("username");
         // String password = getProperty("password"); // if needed
 
-        if (username == null || username.isEmpty()) {
-            log.error("Username is missing in properties file.");
-            throw new IllegalStateException("Missing required property: username");
-        }
-
-        try {
-            _login_page.username_textbox.sendKeys(username);
-            // _login_page.password_textbox.sendKeys(password); // if password is needed
-            _login_page.login_button.click();
-            log.info("User logged in successfully with username: {}", username);
-        } catch (Exception e) {
-            log.error("Login failed: {}", e.getMessage(), e);
-            throw e;
-        }
+//        if (username == null || username.isEmpty()) {
+//            log.error("Username is missing in properties file.");
+//            throw new IllegalStateException("Missing required property: username");
+//        }
+//
+//        try {
+//            _login_page.username_textbox.sendKeys(username);
+//            // _login_page.password_textbox.sendKeys(password); // if password is needed
+//            _login_page.login_button.click();
+//            log.info("User logged in successfully with username: {}", username);
+//        } catch (Exception e) {
+//            log.error("Login failed: {}", e.getMessage(), e);
+//            throw e;
+//        }
     }
 }
